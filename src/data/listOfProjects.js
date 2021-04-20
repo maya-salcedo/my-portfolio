b8a8eca3461dd469e971fa2d03cdb2e551816b63
maya <maya.salcedo07@gmail.com>
2021-04-20 22:03:38 +0300

    added new project

--- src/data/listOfProjects.js
+++ src/data/listOfProjects.js
@@ -8,11 +8,19 @@ const listOfProjects = [
     gitHubURL: "https://github.com/maya-salcedo/Wedding-Invitation-React"
   },
   {
+    nameOfProject: "To-Do App",
+    projectUrl: "https://itsmytodolist.herokuapp.com/",
+    imageOfproject: "image/projects/todolist-img.PNG",
+    description1: "This is a simple to-do app with database.",
+    description2: "React, Node, Express and PostgreSQL are among the technologies used in this project.",
+    gitHubURL: "https://github.com/maya-salcedo/ToDo-App"
+  },
+  {
     nameOfProject: "Offline Dictionary",
-    projectUrl: "https://gofile.io/d/ud3MBM",
+    projectUrl: "",
     imageOfproject: "image/projects/offlineDictionary-img.PNG",
     description1: "This is an offline English dictionary mobile application created using Kivy, an open source Python framework for rapid development of applications that make use of innovative user interfaces. Data are stored in a .json file so no internet connection is needed after downloading.",
-    description2: "The app can be installed by downloading the APK file at the above link.",
+    description2: "",
     gitHubURL: "https://github.com/maya-salcedo/offline-english-dictionary-kivy-app"
   }
 ] 

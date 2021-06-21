69b21eec341b7f50685a7e9719a38017f85845f5
maya <maya.salcedo07@gmail.com>
2021-06-21 22:22:14 +0300

    added skills logo

--- src/components/SkillLogos.jsx
+++ src/components/SkillLogos.jsx
@@ -1,20 +1,5 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-// import React_logo from 'image/logos/React_logo.png';
-// import Javascript_logo from 'image/logos/JavaScript_logo.png';
-// import CSS_logo from 'image/logos/CSS_logo.png';
-// import Express_logo from 'image/logos/Express_logo.PNG';
-// import Node_logo from 'image/logos/Node_logo.png';
-// import Docker_logo from 'image/logos/Docker_logo.png';
-// import HTML5_logo from 'image/logos/HTML5_logo.png';
-// import Postgresql_logo from 'image/logos/Postgresql_logo.png';
-// import Pycharm_logo from 'image/logos/Pycharm_logo.png';
-// import Python_logo from 'image/logos/Python_logo.png';
-// import Vscode_logo from 'image/logos/Vscode_logo.png';
-// import Github_logo from 'image/logos/Github_logo.png';
-// import Bootstrap_logo from 'image/logos/Bootstrap_logo.png';
-// import Heroku_logo from 'image/logos/Heroku_logo.png';
-
 
 const SkillWrapper = styled.div`
   display: flex;
@@ -22,13 +7,13 @@ const SkillWrapper = styled.div`
   item-align: center;
   justify-content: center;
   padding: 10% 20%;
-  @media(max-width: 1140px){
+  @media (max-width: 1140px) {
     padding: 7% 15%;
   }
-  @media(max-width: 999px){
+  @media (max-width: 999px) {
     padding: 5% 10%;
   }
-  @media(max-width: 355px){
+  @media (max-width: 355px) {
     padding: 5% 3%;
   }
 `;
@@ -44,7 +29,7 @@ const rotate = keyframes`
 
 const Card = styled.div`
   width: 7rem;
-  height: 7rem; 
+  height: 7rem;
   &:hover {
     background: linear-gradient(#5eaaa8, #a3d2ca, #f8f1f1);
     animation: ${rotate} 1s linear infinite;
@@ -58,23 +43,39 @@ const LogoWrapper = styled.img`
   padding: 1rem;
 `;
 
-const listOfLogo = ["React_logo", "JavaScript_logo", "CSS_logo", "HTML5_logo", "Node_logo",  "Express_logo", "Docker_logo",  "Postgresql_logo",  "Vscode_logo", "Github_logo", "Heroku_logo", "Bootstrap_logo", "Python_logo", "Pycharm_logo" ];
+const listOfLogo = [
+  'React_logo',
+  'JavaScript_logo',
+  'CSS_logo',
+  'HTML5_logo',
+  'Redux_logo',
+  'Node_logo',
+  'Express_logo',
+  'Docker_logo',
+  'Postgresql_logo',
+  'Mongodb_logo',
+  'Vscode_logo',
+  'Github_logo',
+  'Heroku_logo',
+  'Bootstrap_logo',
+  'Python_logo',
+  'Pycharm_logo',
+];
 
 const SkillLogos = () => {
-  return(
+  return (
     <SkillWrapper>
       {listOfLogo.map((logo, index) => {
-        return(
+        return (
           <div key={logo}>
-          <Card className="card"  >
-            <LogoWrapper className="logo" src={`image/logos/${logo}.png`} />
-          </Card>
-          </div> 
-        ) 
-        })
-      }
+            <Card className="card">
+              <LogoWrapper className="logo" src={`image/logos/${logo}.png`} />
+            </Card>
+          </div>
+        );
+      })}
     </SkillWrapper>
   );
-}
+};
 
-export default SkillLogos; 
+export default SkillLogos;

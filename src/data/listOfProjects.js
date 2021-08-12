3280fc3014bb50a69035ac2ec699c338635ba3eb
maya <maya.salcedo07@gmail.com>
2021-08-12 11:51:51 +0300

    updated project details

--- src/data/listOfProjects.js
+++ src/data/listOfProjects.js
@@ -1,23 +1,23 @@
 const listOfProjects = [
   {
-    nameOfProject: 'Wedding Invitation',
-    projectUrl: 'https://www.emanueleandmayawedding.com/',
-    imageOfproject: 'image/projects/invitation-img.PNG',
-    description1:
-      'This is a full-stack online wedding invitation app that has RSVP feature where invited guests confirm their attendance through the website and the data is saved on the database. They will also get a confirmation email after sending their RSVP.',
-    description2:
-      'React, Node, Express, PostgreSQL, Nodemailer, Google API, Docker are among the technologies used in this project.',
-    gitHubURL: 'https://github.com/maya-salcedo/Wedding-Invitation-React',
-  },
-  {
     nameOfProject: 'Escooter Shop',
     projectUrl: '',
     imageOfproject: 'image/projects/shop-img.PNG',
     description1:
-      'A fullstack e-commerce app that is similar to Amazon web app where you can register and log-in as a user to buy and/or sell products on the app. Products can be searched, sorted, filtered, rated and reviewed. There is also a chatbox that can be used by the buyer to communicate with the seller. Sellers are ranked and the top sellers get featured.',
+      'A fullstack e-commerce app that is similar to Amazon web app where you can register and log-in as a user to buy and/or sell products on the app. Products can be searched, sorted, filtered, rated and reviewed. There is also a chatbox that can be used by the buyer to communicate with the seller. Sellers are ranked and the top sellers get featured. The app is screen-size responsive.',
     description2:
       'React, Node, Express, MongoDB, Redux and SocketIO are among the technologies used in this project.',
     gitHubURL: 'https://github.com/maya-salcedo/escooter-shop',
+  },
+  {
+    nameOfProject: 'Wedding Invitation',
+    projectUrl: 'https://www.emanueleandmayawedding.com/',
+    imageOfproject: 'image/projects/invitation-img.PNG',
+    description1:
+      'This is a full-stack online wedding invitation app that has RSVP feature where invited guests confirm their attendance through the website and the data is saved on the database. They will also get a confirmation email after sending their RSVP. The app is screen-size responsive.',
+    description2:
+      'React, Node, Express, PostgreSQL, Nodemailer, Google API and Docker are among the technologies used in this project. It is deployed through Heroku.',
+    gitHubURL: 'https://github.com/maya-salcedo/Wedding-Invitation-React',
   },
   {
     nameOfProject: 'To-Do App',

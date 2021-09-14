69079990b048a2040a3159fe74f65d3315f3d68f
maya <maya.salcedo07@gmail.com>
2021-09-14 14:17:39 +0300

    fixing the bug in mobile view

--- src/elements/GlobalStyle.jsx
+++ src/elements/GlobalStyle.jsx
@@ -1,30 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-html, body {
-  font-family: 'Sacramento', cursive;
-  margin: 0px;
-  padding: 0px;
-  width: 100%;
-  height: 100%;
-  overflow-x: hidden; 
-}
+  html, body {
+    font-family: 'Sacramento', cursive;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+  }
 
-h2 {
-  font-family: 'Source Sans Pro', sans-serif;
-    
-}
+  h2 {
+    font-family: 'Source Sans Pro', sans-serif;
+      
+  }
 
-p, a {
-  font-family: 'Merriweather', serif;
-}
+  p, a {
+    font-family: 'Merriweather', serif;
+  }
 
-.close-icon:hover, .close-icon:active {
-  transform: rotate( 180deg );            
-  transition: transform 1s ease;
-  color: #14274E;
-}
-
+  .close-icon:hover, .close-icon:active {
+    transform: rotate( 180deg );            
+    transition: transform 1s ease;
+    color: #14274E;
+  }
 `;
 
 export default GlobalStyle;
